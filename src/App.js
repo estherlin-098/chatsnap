6d364f90472700a5580d50d8ca2e3c16802e2367
QIEN LIN <qienlin@QIENdeMacBook-Pro.local>
2020-01-17 09:14:54 -0800

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return <main>
+  
+    <header>
+      <img className="logo"
+        alt="logo"
+        src="https://www.plantronics.com/etc/designs/plantronics/clientlib-all/img/poly-logo.png"
+      />
+      Chatter
+    </header>
+  
+    <TextInput onSend={t=> console.log(t)} />
+  
+  </main>
+ }
+ 
+ function TextInput(props) {
+   const [text,setText] = useState('') //const[variable,function]
+ 
+   return <div className = "text-input">
+     <input value={text} 
+     placeholder = "Write your message" 
+     onChange = {e=> setText(e.target.value)} //e = event, capture the input
+     /> 
+ 
+     <button className = "button" 
+       onClick={()=> {
+       props.onSend(text)
+       setText('')
+       }}> 
+       <img className = "sendButton" 
+       src="https://static.thenounproject.com/png/59763-200.png" />
+     </button>
+   </div>
+ }
+ 
 
 export default App;
